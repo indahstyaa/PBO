7f38a685607c9f7e0de9d84d8e68596b6d8ef346
indahstyaa <indahsetyaningrum011@gmail.com>
2025-03-06 16:24:44 +0700

    First commit

---
+++ week1/Tugas1.java
@@ -0,0 +1,31 @@
+package week1;
+
+class Tugas1{
+    public static void main(String[] biodata){
+        String nim;
+        nim = "A11.2023.15403";
+        String nama;
+        nama = "Indah Setyaningrum";
+        String alamat;
+        alamat = "Ds. Kecapi, Kec. Tahunan, Kab. Jepara";
+        String kota;
+        kota = "Jepara";
+        String kodepos;
+        kodepos = "59429";
+        String telp;
+        telp = "082134562717";
+        String hp;
+        hp = "082134562717";
+        String email;
+        email = "111202315403@mhs.dinus.ac.id";
+
+        System.out.println("Nim :"+nim);
+        System.out.println("Nama :"+nama);
+        System.out.println("alamat :"+alamat);
+        System.out.println("kota :"+kota);
+        System.out.println("kodepos :"+kodepos);
+        System.out.println("telp :"+telp);
+        System.out.println("hp :"+hp);
+        System.out.println("email :"+email);
+    }
+}
---
+++ week1/Tugas2persegi.java
@@ -0,0 +1,14 @@
+package week1;
+
+class Tugas2persegi{
+    public static void main(String[] persegi){
+        int sisi = 5;
+        int luas = sisi * sisi;
+        int keliling = 4 * sisi;
+
+        System.out.println("program menghitung luas dan keliling persegi");
+        System.out.println("sisi                   :"+sisi);
+        System.out.println("luas(sisi * sisi)      :"+luas);
+        System.out.println("keliling(4 * sisi)     :"+keliling);
+    }
+}
